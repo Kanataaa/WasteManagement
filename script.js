73b5b6c78fbad6808a7e0bb6d987426c316bc998
Kanataaa <cqnfinedmc@gmail.com>
2021-08-16 13:56:18 -0400

    Percentage added to 2nd option.

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let amtDiv = document.querySelector("#amtDiv");
 
 let choiceDiv = document.querySelector("#choice");
 let ugoalDiv = document.querySelector("#userGoal");
+let wgoalDiv = document.querySelector("#webGoal");
 
 
 let catagoryInput = document.querySelector("#catagory");
@@ -18,8 +19,9 @@ let newBreak = document.createElement("hr");
 let userGoal = document.createElement("button");
 let webGoal = document.createElement("button");
 
-let perText = document.createElement("p");
+let perSymb = document.createElement("p");
 let gText = document.createElement("p");
+let perText = document.createElement("p");
 
 let goalInput = document.createElement("input");
 
@@ -30,11 +32,14 @@ submitOneButton.onclick = function(event) {
     ugoalDiv.style.display = "";
     ugoalDiv.innerHTML = "";
 
+    wgoalDiv.style.display = "";
+    wgoalDiv.innerHTML = "";
+
     if (catagoryInput.value != "") {
         choiceDiv.appendChild(newBreak);
 
         userGoal.innerHTML = "I would like to choose my own goal.";
-        webGoal.innerHTML = "I would like the website to choose my goal."
+        webGoal.innerHTML = "I would like the website to choose my goal.";
 
         choiceDiv.appendChild(userGoal);
         choiceDiv.appendChild(webGoal);
@@ -44,13 +49,33 @@ submitOneButton.onclick = function(event) {
             webGoal.remove();
 
             goalInput.setAttribute("placeholder", "Please input a percentage you want to reduce.")
-            perText.innerHTML = "%";
-            perText.style.fontSize = "19px";
+            goalInput.setAttribute("type", "number");
+
+            perSymb.innerHTML = "%";
+            perSymb.style.fontSize = "19px";
 
             ugoalDiv.style.display = "flex";
 
             ugoalDiv.appendChild(goalInput);
-            ugoalDiv.appendChild(perText);
+            ugoalDiv.appendChild(perSymb);
+
+            goalInput.addEventListener("keyup", function(event){
+                if (event.key === "Enter") {
+                    if (goalInput.value != "") {
+                        
+                        let num = (catagoryInput.value/100)*goalInput.value;
+                        let userTrashGoal = num.toFixed(1);
+                        perText.innerHTML = "You have set your reduction percentage to " + goalInput.value + "%";  
+                        gText.innerHTML = "Your goal will be to reduce trash down to only " + userTrashGoal + "kg!";
+
+                        ugoalDiv.appendChild(perText);
+                        ugoalDiv.appendChild(gText);
+                    } else {
+                        alert("Please input a number for your percentage.")
+                    }
+                    
+                }
+            })
 
         }
 
@@ -58,20 +83,22 @@ submitOneButton.onclick = function(event) {
             console.log("We set the users goal.")
             userGoal.remove();
 
-            let webTrashGoal = catagoryInput.value - Math.floor(catagoryInput.value * 0.3);
+            let num = catagoryInput.value - (catagoryInput.value * 0.3);
+            let webTrashGoal = num.toFixed(1);
 
             perText.innerHTML = "We will automatically set your reduction percentage to 30%!";
             gText.innerHTML = "Your goal will be to reduce trash down to only " + webTrashGoal + "kg!";
+
             perText.style.fontSize = "22px";
             gText.style.fontSize = "22px";
 
-            ugoalDiv.appendChild(perText);
-            ugoalDiv.appendChild(gText);
+            wgoalDiv.appendChild(perText);
+            wgoalDiv.appendChild(gText);
         }
 
 
     } else {
-        alert("Please input the weight of your trash (kg) in the field.")
+        alert("Please input an approximate weight of your trash in kilograms.")
     }
 
 }
