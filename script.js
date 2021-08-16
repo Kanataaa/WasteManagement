cc99a4bc1f2c3b47bf39e2017525f0e3f9142407
Kanataaa <cqnfinedmc@gmail.com>
2021-08-16 12:00:47 -0400

    Basic layout of website.

---
+++ script.js
@@ -0,0 +1,73 @@
+// Declaring Variables
+let subtext = document.querySelector(".subtext");
+let submitOneButton = document.querySelector("#submitOne");
+let amtDiv = document.querySelector("#amtDiv");
+
+let choiceDiv = document.querySelector("#choice");
+let ugoalDiv = document.querySelector("#userGoal");
+
+
+let catagoryInput = document.querySelector("#catagory");
+let cText = document.querySelector("#cText")
+
+
+// New inputs.
+
+let newBreak = document.createElement("hr");
+
+let userGoal = document.createElement("button");
+let webGoal = document.createElement("button");
+
+let perText = document.createElement("p");
+let gText = document.createElement("p");
+
+let goalInput = document.createElement("input");
+
+// Functions
+
+submitOneButton.onclick = function(event) {
+    event.preventDefault();
+
+    if (catagoryInput.value != "") {
+        choiceDiv.appendChild(newBreak);
+
+        userGoal.innerHTML = "I would like to choose my own goal.";
+        webGoal.innerHTML = "I would like the website to choose my goal."
+
+        choiceDiv.appendChild(userGoal);
+        choiceDiv.appendChild(webGoal);
+
+        userGoal.onclick = function(event) {
+            console.log("User sets their own goal.");
+            webGoal.remove();
+
+            goalInput.setAttribute("placeholder", "Please input a percentage you want to reduce.")
+            perText.innerHTML = "%";
+            perText.style.fontSize = "19px";
+
+            ugoalDiv.appendChild(goalInput);
+            ugoalDiv.appendChild(perText);
+
+        }
+
+        webGoal.onclick = function(event) {
+            console.log("We set the users goal.")
+            userGoal.remove();
+
+            let webTrashGoal = Math.floor(catagoryInput.value * 0.3) 
+
+            perText.innerHTML = "We will automatically set your reduction percentage to 30%!";
+            gText.innerHTML = "Your goal will be to reduce trash to: " + webTrashGoal + " kg from " + catagoryInput.value + "!";
+            perText.style.fontSize = "22px";
+            gText.style.fontSize = "22px";
+
+            ugoalDiv.appendChild(perText);
+            ugoalDiv.appendChild(gText);
+        }
+
+
+    } else {
+        alert("Please input the weight of your trash (kg) in the field.")
+    }
+
+}
