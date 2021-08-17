d45381c7d968f75316fa34d495e303fba92b67c5
Kanataaa <cqnfinedmc@gmail.com>
2021-08-17 11:45:05 -0400

    Added append system to user interface.

--- manage.js
+++ manage.js
@@ -3,10 +3,31 @@ let weightPrediction = localStorage.getItem("weightPrediction");
 let percReduc = localStorage.getItem("percReduc");
 let TTO = localStorage.getItem("TrashGoalOne");
 
+// Doc Variables
 
+// Buttons
+let submitButton = document.querySelector("#append");
+
+// Inputs
+let typeInput = document.querySelector("#type");
+let weightInput = document.querySelector("#weight");
+let amountInput = document.querySelector("#amount");
+let dateInput = document.querySelector("#date");
+
+// Divs
+let typeDiv = document.querySelector(".typeAppend");
+let weightDiv = document.querySelector(".weightAppend");
+let amountDiv = document.querySelector(".amountAppend");
+let dateDiv = document.querySelector(".dateAppend");
+
+// Test Info Transfer
 console.log(weightPrediction);
 console.log(percReduc);
 console.log(TTO);
+
+submitButton.onclick = function() {
+    
+}
 
 
 
